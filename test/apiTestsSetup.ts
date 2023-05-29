d35e36ba677eeefb90dc944453121fd5a1c7dd90
Elad Kohavi <106978846+EladKohavi@users.noreply.github.com>
2023-05-29 15:53:18 +0300

    Update apiTestsSetup.ts

--- test/apiTestsSetup.ts
+++ test/apiTestsSetup.ts
@@ -5,6 +5,9 @@
 
 import server = require('./../server')
 console.log("hola hola");
+console.log("hola hola");
+console.log("hola hola");
+
 // eslint-disable-next-line no-async-promise-executor,@typescript-eslint/no-misused-promises
 export = async () => await new Promise<void>(async (resolve, reject) =>
   await server.start((err: Error) => {
