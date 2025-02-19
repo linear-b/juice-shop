2390e025f1aed8c3bbf40bc2fa86d948bf6008cf
Pavel Vaks <129676672+PavelLinearB@users.noreply.github.com>
2025-02-19 10:32:46 +0200

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2014-2024 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
- */
-
 /* jslint node: true */
 import packageJson from '../package.json'
 import fs from 'fs'
